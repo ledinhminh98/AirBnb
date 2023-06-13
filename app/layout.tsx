db589c1232d846e08fcb9982b03cbbf5a50ac39f
Minh Le <mle@guidedflow.io>
2023-06-13 07:04:06 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import { Nunito } from "next/font/google";
+
+import LoginModal from "@/app/components/modals/LoginModal";
+import RegisterModal from "@/app/components/modals/RegisterModal";
+import RentModal from "@/app/components/modals/RentModal";
+import SearchModal from "@/app/components/modals/SearchModal";
+import Navbar from "@/app/components/navbar/Navbar";
+
+import ToasterProvider from "@/app/providers/ToasterProvider";
+
+import getCurrentUser from "./actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
+import "./globals.css";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
+}
